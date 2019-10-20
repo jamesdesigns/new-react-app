83c57fecadbaae2fcb362a9e72ea0a1dac15fbe1
James Hooper <jameshooperdesigns@gmail.com>
2019-10-20 15:54:27 -0600

    Add Hello World to App.js

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>New React App</Text>
+      <Text>Hello World!</Text>
     </View>
   );
 }
